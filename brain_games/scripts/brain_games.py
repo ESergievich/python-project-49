e6b39340137eaf859f3bb53184e7f01f217e8615
ESergievich <sergievichevgeniy@gmail.com>
2023-06-10 17:18:12 +0300

    step4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,10 +4,10 @@ from brain_games.cli import welcome_user
 
 
 def main():
-	print('poetry run python -m brain_games.scripts.brain_games')
-	print('Welcome to the Brain Games!')
-	welcome_user()
+    print('poetry run python -m brain_games.scripts.brain_games')
+    print('Welcome to the Brain Games!')
+    welcome_user()
+
 
 if __name__ == '__main__':
-	main()
-
+    main()
